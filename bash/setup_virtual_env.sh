9c287ae52c48b759c3cc01e08a5f3a60623d36fd
alex <alexhldmn5@gmail.com>
2019-11-24 20:55:12 -0500

    Finalizes (for now) setup_virtual_env.sh and ignores swp files in gitignore

--- bash/setup_virtual_env.sh
+++ bash/setup_virtual_env.sh
@@ -5,19 +5,21 @@ if [ "$1" = "" ] || [ $# -lt 1 ]; then
 	echo "Error: missing virtual environment name"
 	echo $usage_message
 	exit
-elif [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-	echo $usage_mesage
+fi
+	
+if [[ "$1" = *"--help"* ]] || [[ "$1" = *"-h"* ]]; then
+	echo $usage_message
 fi
 
-exit
 
-output=$(python3 -m venv hello-template-env)
+virtual_env_name=$1
+output=$(python3 -m venv $virtual_env_name)
 success_message="Virtual Environment created successfully"
 
 if [ $? -ne 0 ]; then
 	echo "python3-venv not installed, installing now..."
 	sudo apt-get install python3-venv
-	python3 -m venv hello-template-env
+	python3 -m venv $virtual_env_name
 	if [ $? -e 0 ]; then 
 		echo $success_message
 	else 
